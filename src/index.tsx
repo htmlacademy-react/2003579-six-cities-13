a09b2fd7a0ce27801ef8a7c678bb672f6798b0c0
Alina Arbekova <barankignu@yandex.ru>
2023-07-13 18:47:33 +0300

    Начинаем программировать

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const ACCOMODATIONNUMBER = 5;
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App accomodationNumber={ACCOMODATIONNUMBER}></App>
   </React.StrictMode>
 );
