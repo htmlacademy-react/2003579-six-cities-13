0994aac8de8be6ba3f29f3d9d9c0c3e61d89e726
Alina Arbekova <barankignu@yandex.ru>
2023-07-20 02:09:52 +0300

    Маршрут по тайным тропам

---
+++ src/components/login-check/login-check.tsx
@@ -0,0 +1,17 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { AuthorizationStatus } from '../../const';
+import { AppRoute } from '../../const';
+
+type LoginCheckProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function LoginCheck(props: LoginCheckProps): JSX.Element {
+  const { authorizationStatus, children } = props;
+  return (
+    authorizationStatus === AuthorizationStatus.Auth ? children : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default LoginCheck;
