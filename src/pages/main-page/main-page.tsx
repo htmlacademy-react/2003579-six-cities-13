82dc4bac24f1c72ca2de5feb3500374b61baaa03
Alina Arbekova <barankignu@yandex.ru>
2023-08-01 03:03:11 +0300

    Практика в разрешении конфликтов

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -98,9 +98,7 @@ function MainPage({offersData}: MainPageProps): JSX.Element {
                   <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
-              <div className="cities__places-list places__list tabs__content">
-                {[...Array(offersData.length).keys()].map((item: number) => <AccomodationCard {...offersData[item]} key={offersData[item].id} />)}
-              </div>
+              <OffersList offersData={offersData} />
             </section>
             <div className="cities__right-section">
               <section className="cities__map map"></section>
